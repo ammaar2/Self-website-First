b3dde8ac3c1b635f7e04752182ed05a5877df616
𝐀𝐈𝐙𝐄𝐍 <ammerrafa@gmail.com>
2025-02-03 00:55:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
